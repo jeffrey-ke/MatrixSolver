f67e6d2551da71ac411e40494eb0e32192de8b8a
Jeffrey Ke <jeffke613@gmail.com>
2022-05-05 09:26:47 -0700

    Made sub-components for the title of the webpage and the input fields.

--- proj/src/App.js
+++ proj/src/App.js
@@ -1,25 +1,16 @@
 import "./App.css";
+import Message from "./Message";
+import Input from "./Input";
 
 function App() {
   return (
     <div className="App">
-      <h1>Matrix row reducer:</h1>
-      <h2>Enter # of rows and cols:</h2>
-      <form action="matrix" method="post">
-        rows:{" "}
-        <input
-          type="number"
-          name="rows"
-          value=""
-          autofocus="autofocus"
-          required
-        ></input>
-        <br></br>
-        cols:{" "}
-        <input type="number" name="cols" value="" tabindex="0" required></input>
-        <br></br>
-        <input type="submit" value="submit"></input>
-      </form>
+      {/* message goes here */}
+      <Message
+        title="Matrix Row Reducer"
+        instruction="Enter the number of rows and columns."
+      />
+      <Input dst='matrix'/>
     </div>
   );
 }
---
+++ proj/src/Message.js
@@ -0,0 +1,11 @@
+
+function Message(props){
+    return(
+        <div>
+            <h1>{props.title}</h1>
+            <h2>{props.instruction}</h2>
+        </div>
+    )
+}
+
+export default Message
