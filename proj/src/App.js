7f77f27771d3554b2af2170c6cd61c331a255562
Jeffrey Ke <jeffke613@gmail.com>
2022-05-08 18:18:05 -0700

    Changed to using buttons to edit the number of rows and columns and used state hooks to re-render my matrix each time a button is clicked. A much simpler implementation.

--- proj/src/App.js
+++ proj/src/App.js
@@ -1,19 +1,29 @@
-// import React, { useState } from 'react'
+import React, { useState } from 'react'
 import "./App.css";
 import Message from "./Message";
-import Input from "./Input";
+// import Input from "./Input";
 import Matrix from './Matrix';
 
 function App() {
+
+  const [row,setRow] = useState(2)
+  const [col,setCol] = useState(2)
+
   return (
     <div className="App">
       <Message
         title="Matrix Row Reducer"
-        instruction="Enter the number of rows and columns."
+        instruction=""
       />
-      {/* There should be two states: one for each input field for rows and cols */}
-      <Input dst='matrix'/>
-      <Matrix rows={4} cols={4}/>
+      <div>
+        <h3>Row</h3><br/>
+        <button onClick={()=>{setRow(row + 1)}}>+</button><button onClick={()=>{setRow(row - 1)}}>-</button>
+
+        <h3>Col</h3><br/>
+        <button onClick={()=>{setCol(col + 1)}}>+</button><button onClick={()=>{setCol(col-1)}}>-</button>
+
+      </div>
+      <Matrix rows={row} cols={col}/>
     </div>
   );
 }
--- proj/src/Input.js
+++ proj/src/Input.js
@@ -2,27 +2,7 @@ function Input(props) {
 
     return (
         <div>
-            <form action={props.dst} method="post">
-                rows:{" "}
-                <input
-                    type="number"
-                    name="rows"
-                    value=""
-                    autofocus="autofocus"
-                    required
-                />
-                <br/>
-                cols:{" "}
-                <input 
-                    type="number" 
-                    name="cols" 
-                    value="" 
-                    tabindex="0" 
-                    required
-                />
-                <br/>
-                <input type="submit" value="submit"/>
-            </form>
+            
         </div>
     )
 }
