81a756b30b3eabdc3034e0669e7724505e4586a4
Jeffrey Ke <jeffke613@gmail.com>
2022-05-17 20:01:16 -0700

    Attempting an elaborate method of sending my matrix data to the flask backend with a 'fetch' request + a 'body' field; currently trying to determine how to get flask to correctly interpret the request, including which functions I have to use to interpret the body json.

--- proj/src/Matrix.js
+++ proj/src/Matrix.js
@@ -2,21 +2,46 @@
 
 export default function Matrix(props){
 
-    function generateMatrix(){
+    const generateMatrix = () =>{
         const m = []
         for (let i = 0; i < props.rows; i++) {
             for (let j = 0; j < props.cols; j++){
-                m.push(<input type="number" name="cell"/>)
+                m.push(<input type="number" required name="cell"/>)
             }
             m.push(<br/>)
         }
         return m
     }
 
+    const compute = () =>{
+        let inputs = document.getElementsByName('cell') //this hands back an array of all my input cells
+        const str_cells = []
+        inputs.forEach(cell => {
+            str_cells.push((cell.value == null)? 0 : cell.value)
+        })
+        document.getElementById('values').value = str_cells.join(' ')
+        fetch('/result', {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: {
+                'values': str_cells,
+                'rows': rows,
+                'cols': cols
+            }
+        }).then(response => response.json()).then(data => {
+            // json format of the fetch format can be pretty simple:
+            // {
+                // 'matrix' : 'matrix cell data strung together in a string'
+            // }
+        })
+    }
 
     return (
         <div>
             { generateMatrix() }
+            <button onClick={() => compute()}>submit</button>
+            
         </div>
     )
 
